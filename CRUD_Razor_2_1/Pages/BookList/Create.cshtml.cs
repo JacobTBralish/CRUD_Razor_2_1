15d1a08d7748db1ed1a945fb23bd367bbf176d50
Jacob <j.t.bralish@gmail.com>
2018-12-19 00:34:18 -0800

    Added create metod to the project
    Create working successfully

---
+++ CRUD_Razor_2_1/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Razor_2_1.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor_2_1.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // Bind property
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        //Post handler. If we do not want to retrive the Book object from the razor page we can do a bindproperty shown above
+        public async Task<IActionResult> OnPost(/*Book Book*/)
+        {
+            /*1. ModelState tells you if an model errors have been added to ModelState. The default model binder will add
+             some errors for basic type conversion issues (ex. passing a non-number for something which is an int). You can
+             populate ModelState more fully based on whichever validation you're using.
+             2. Indecates if it was possible to bind the incoming values from the request to the model*/
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Books.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
--- CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
+++ CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
@@ -2,17 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUD_Razor_2_1.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_Razor_2_1.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public string someData { get; set; }
-        public void OnGet()
+        /* The instance of the database is set to readonly private because this
+         * instance should be kept confined to this component only */
+        private readonly ApplicationDbContext _db;
+        /* Set to IEnumerable so we are able to invoke "foreach" upon the data for displaying. IEnumerable provides a 
+         a method "IEnumerator" which returns an IEnumerator and provides the ability to iterate through a collection by 
+         exposing the current property and MoveNext and Reset methods.
+         *****SIMPLE TERMS: IEnumerable makes using foreach possible on the collection*/
+        public IEnumerable<Book> Books { get; set; }
+
+        // This is creating an instance of the database so that we can retrive it,
+        public IndexModel(ApplicationDbContext db)
         {
-            someData = "This is my first property";
+            _db = db;
+        }
+        // To make async you must add async Task here to get the data
+        public async Task OnGet()
+        {
+            /* Async method used to get the books and stored to the IEnumerable.
+             _db.Books is coming from the ApplicationDbContext which is the DbSet.*/
+            Books = await _db.Books.ToListAsync();
         }
     }
 }
